fd3e1247c707290c0e708f26760688f4362e6ea7
Marc <malcover@iesfuentesanluis.org>
2026-02-10 01:46:21 +0100

    CI/CD deploy with Ansible5

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-app.get("/", (req, res) => res.send("Biienvenido a la página de Marc,Aupa Alba\n"));
+app.get("/", (req, res) => res.send("Biieenvenido a la página de Marc,Aupa Alba\n"));
 app.listen(port, () => console.log(`Listening on ${port}`));
