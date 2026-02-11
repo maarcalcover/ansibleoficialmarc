bd09ecae380ac8024f882c138b3b231803e5bc7d
Tu Nombre <you@example.com>
2026-02-11 15:19:08 +0100

    change puerto

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 2654;
 app.get("/", (req, res) => res.send("Examen DAW Despliegue inicial\n"));
 app.listen(port, () => console.log(`Listening on ${port}`));
